18a152d56711f8e5cfdd054ab2369c1cc2c26f93
DevNovaes <viniciusnovaes2@gmail.com>
2025-03-01 17:20:48 -0300

    Projeto finalizado

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -3,6 +3,7 @@
 let amigos = [];
 let nome = document.getElementById('amigo');
 let lista = document.getElementById('listaAmigos');
+let sorteado = document.getElementById('resultado');
 
 function adicionarAmigo() {
     if (nome.value == '')
@@ -11,8 +12,18 @@ function adicionarAmigo() {
         amigos.push(nome.value);
         nome.value = '';
         lista.innerHTML = '';
+        sorteado.innerHTML = '';
         for (let i = 0; i < amigos.length; i++){
-            
+            lista.innerHTML += `<li>${amigos[i]}</li>`;
         }
+    }
+}
+
+function sortearAmigo() {
+    if (amigos == '')
+        alert('A lista está vazia');
+    else {
+        let sorteio = Math.floor(Math.random() * amigos.length);
+        sorteado.innerHTML = `O amigo sorteado é ${amigos[sorteio]}`;
     }
 }
